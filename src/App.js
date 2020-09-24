a24bc8196206fb20149970994394c7c5f7c1f3c7
Troy Sargent <troyjsargent@gmail.com>
2020-09-23 22:19:34 -0500

    portfolio layout responsive and button/chips added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/navbar"
 import './App.css';
 
 function App() {
   return (
-    <Container>
+    <>
 
-    </Container>
+    <Router>
+    <Navbar />
+      <Container>
+          <Switch>
+            <Route exact path="/"  />
+            <Route exact path="/resume"  />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact"  />
+          </Switch>
+      </Container>
+    </Router>
+ 
+    </>
   );
 }
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,31 +1,46 @@
 import React from "react";
-import Grid from "../components/Grid";
-import { Paper } from '@material-ui/core';
+import { Card, Grid, CardContent, Typography, CardActions, Button, Chip } from '@material-ui/core';
+import {projects} from "../static/content/Portfolio.json";
+import Image from 'material-ui-image';
 
-export default function Portfolio() {
+function Portfolio() {
     return(
-        <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper>xs=12</Paper>
+        <Grid container spacing={3}> 
+        {projects.map(project => {
+            return (
+        <Grid item xs={12} sm={6} lg={4}>
+          <Card>
+          <CardContent>
+        <Typography align="center" variant="h4" gutterBottom>
+          {project.name}
+        </Typography>
+        <Image src={project.image}/>
+        <Typography align="center" style={{height:"3em"}} variant="body1" component="p">
+        {project.description}
+        </Typography>
+        <div container style={{display: "flex", justifyContent: 'space-around', marginTop: "1em"}}>
+        {project.technologies.map(tech => {
+            return (
+                <Chip 
+                margin="1em"
+                label={tech}
+                color="primary"
+                />
+            )
+        })} 
+        </div>
+      </CardContent>
+      <CardActions style={{justifyContent: 'center'}}>
+        <Button href="https://www.google.com" color="primary" variant="outlined"size="medium">Github</Button>
+        <Button color="primary" variant="outlined"size="medium">Demo</Button>
+      </CardActions>
+          </Card>
         </Grid>
-        <Grid item xs={6}>
-          <Paper>xs=6</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>xs=3</Paper>
-        </Grid>
+            )
+        })}
+
       </Grid>
     )
-}
+};
+
+export default Portfolio;
