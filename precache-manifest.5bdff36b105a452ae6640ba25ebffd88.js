24b9b5d9fad9a4d7216cebe77acefe6000c3627f
Troy Sargent <troyjsargent@gmail.com>
2020-09-24 11:45:23 -0500

    Updates

---
+++ precache-manifest.5bdff36b105a452ae6640ba25ebffd88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50848497e0f94fcf0fc96e2901eda850",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5cf5f7d2334aa3da2ebf",
+    "url": "/react-portfolio/static/css/main.b8162f42.chunk.css"
+  },
+  {
+    "revision": "97df48181bfb0c062b9f",
+    "url": "/react-portfolio/static/js/2.9865886e.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/react-portfolio/static/js/2.9865886e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf5f7d2334aa3da2ebf",
+    "url": "/react-portfolio/static/js/main.04ce52a0.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "fa9b4c568f6664e5009caa45ca82efdf",
+    "url": "/react-portfolio/static/media/background.fa9b4c56.jpg"
+  },
+  {
+    "revision": "c8f43482ec74749fee369a7763abf7b9",
+    "url": "/react-portfolio/static/media/profile.c8f43482.jpeg"
+  }
+]);
