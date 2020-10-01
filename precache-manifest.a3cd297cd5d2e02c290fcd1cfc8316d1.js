e97efcb44f300e6e217aec6a7a09d182c67013a0
Troy Sargent <troyjsargent@gmail.com>
2020-10-01 09:55:19 -0500

    Updates

---
+++ precache-manifest.a3cd297cd5d2e02c290fcd1cfc8316d1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e3bcb52082ff341078af5c133d34aa",
+    "url": "/www.troysargent.com/index.html"
+  },
+  {
+    "revision": "95c3f82598f2b969ef64",
+    "url": "/www.troysargent.com/static/css/main.c16e7276.chunk.css"
+  },
+  {
+    "revision": "f2da2930bca1bd9523ed",
+    "url": "/www.troysargent.com/static/js/2.55ed8057.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/www.troysargent.com/static/js/2.55ed8057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c3f82598f2b969ef64",
+    "url": "/www.troysargent.com/static/js/main.1dedf5d6.chunk.js"
+  },
+  {
+    "revision": "319778c02b62e65112e0",
+    "url": "/www.troysargent.com/static/js/runtime-main.9131ae0e.js"
+  },
+  {
+    "revision": "fa9b4c568f6664e5009caa45ca82efdf",
+    "url": "/www.troysargent.com/static/media/background.fa9b4c56.jpg"
+  },
+  {
+    "revision": "e64ff332ac7a015d2a7a4e6992128f9d",
+    "url": "/www.troysargent.com/static/media/birdie.e64ff332.png"
+  },
+  {
+    "revision": "b1c896c7c9cae21b4658f9a1a13cf9a9",
+    "url": "/www.troysargent.com/static/media/employeedirectory.b1c896c7.png"
+  },
+  {
+    "revision": "88dcebb3957a1af4926b7205187330e9",
+    "url": "/www.troysargent.com/static/media/fitnesstracker.88dcebb3.png"
+  },
+  {
+    "revision": "abe126c4ae4647f44154163be75620f2",
+    "url": "/www.troysargent.com/static/media/hr.abe126c4.png"
+  },
+  {
+    "revision": "547fe6f76c57bac3dc952bf5cab4d7f4",
+    "url": "/www.troysargent.com/static/media/moneytrend.547fe6f7.png"
+  },
+  {
+    "revision": "c8f43482ec74749fee369a7763abf7b9",
+    "url": "/www.troysargent.com/static/media/profile.c8f43482.jpeg"
+  },
+  {
+    "revision": "3da37840ad3e6ff28ddd28a4cc663201",
+    "url": "/www.troysargent.com/static/media/transactionapp.3da37840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.31e1276f08046893fdbd714dbd1fa067.js"
+  "/www.troysargent.com/precache-manifest.a3cd297cd5d2e02c290fcd1cfc8316d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.troysargent.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
