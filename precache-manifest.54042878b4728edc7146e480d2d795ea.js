29e42834cba1e1aea63d13411eac44d4a6df7fe5
Troy Sargent <troyjsargent@gmail.com>
2020-10-01 10:11:16 -0500

    Updates

---
+++ precache-manifest.54042878b4728edc7146e480d2d795ea.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26cad2949eb92f34ba6c1608167f9f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72166382c97811afa14",
+    "url": "/static/css/main.6368738b.chunk.css"
+  },
+  {
+    "revision": "f2da2930bca1bd9523ed",
+    "url": "/static/js/2.55ed8057.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.55ed8057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72166382c97811afa14",
+    "url": "/static/js/main.1dedf5d6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "fa9b4c568f6664e5009caa45ca82efdf",
+    "url": "/static/media/background.fa9b4c56.jpg"
+  },
+  {
+    "revision": "e64ff332ac7a015d2a7a4e6992128f9d",
+    "url": "/static/media/birdie.e64ff332.png"
+  },
+  {
+    "revision": "b1c896c7c9cae21b4658f9a1a13cf9a9",
+    "url": "/static/media/employeedirectory.b1c896c7.png"
+  },
+  {
+    "revision": "88dcebb3957a1af4926b7205187330e9",
+    "url": "/static/media/fitnesstracker.88dcebb3.png"
+  },
+  {
+    "revision": "abe126c4ae4647f44154163be75620f2",
+    "url": "/static/media/hr.abe126c4.png"
+  },
+  {
+    "revision": "547fe6f76c57bac3dc952bf5cab4d7f4",
+    "url": "/static/media/moneytrend.547fe6f7.png"
+  },
+  {
+    "revision": "c8f43482ec74749fee369a7763abf7b9",
+    "url": "/static/media/profile.c8f43482.jpeg"
+  },
+  {
+    "revision": "3da37840ad3e6ff28ddd28a4cc663201",
+    "url": "/static/media/transactionapp.3da37840.png"
+  }
+]);
